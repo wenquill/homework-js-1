e02f30b98d26eef1f5c74b756c7889cccb9ea5af
victoria <vikabirukova22@gmail.com>
2023-08-15 19:56:51 +0300

    New commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// 1.
+let a = 21;
+let b = 194;
+console.log(a * b);
+
+// 2.
+let c = 802;
+let d = 94;
+console.log(c / d);
+
+// 3.
+let e = 1999493;
+let f = 99129;
+console.log(e + f);
+
+// 4.
+let num = 11;
+let bool = true;
+let str = 'java script';
+let numStr = '100';
+console.log(num, bool, str, numStr);
+
+// 5.
+num = 1;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+num++;
+num--;
+
+// 6.1
+const userNumber = prompt("Your number is");
+console.log(userNumber ** 2);
+
+//6.2
+const userFirstNumber = prompt("The first number:");
+const userSecondNumber = prompt("The second number:");
+console.log((userFirstNumber + userSecondNumber) / 2);
+
+//6.3 
+const userTime = prompt("Minutes:");
+console.log(userTime * 60);
+
+//6.4 
+const greeting = "Hello, ";
+let userName;
+userName = prompt("What is your name?");
+console.log(greeting + userName + "!");
